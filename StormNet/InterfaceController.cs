49fd0d1ef0b3e71008393e1433acf1e1c32d170e
Wouter <seweso@gmail.com>
2021-05-21 16:21:19 +0200

    initial commit

---
+++ StormNet/InterfaceController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace StormNet
+{
+    [Route("Get")]
+    [ApiController]
+    public class InterfaceController : ControllerBase
+    {
+        [HttpGet("/")]
+        public async Task<IActionResult> Get([FromQuery]IDictionary<string, string> query)
+        {
+            Console.WriteLine(DateTime.Now.ToString("O"));
+            Console.WriteLine(JsonConvert.SerializeObject(query, Formatting.Indented));
+            return Ok();
+        }
+    }
+}
