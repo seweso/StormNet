f717059b40ea8b23fd161bf6f7b6afe32968f8bd
Wouter <seweso@gmail.com>
2021-05-23 21:52:53 +0200

    Small changes

--- StormNet/Startup.cs
+++ StormNet/Startup.cs
@@ -18,19 +18,14 @@ namespace StormNet
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-
             services.AddSingleton<DataHandler>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseDeveloperExceptionPage();
             app.UseRouting();
-
             app.UseEndpoints(endpoints => endpoints.MapControllers());
         }
     }
